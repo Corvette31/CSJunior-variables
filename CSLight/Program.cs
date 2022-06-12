afa1f22b808c43cffe35677f5c21173b4d9a24ec
Corvette31 <corvette.ss@yandex.ru>
2022-06-12 20:37:03 +0300

    Добавьте файлы проекта.

---
+++ CSLight/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSLight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 38;
+            int c = (31 - 5 * a) / b ;
+            Console.WriteLine(c);
+        }
+    }
+}
